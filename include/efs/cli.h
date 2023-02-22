e395228e1088732b8a083121c45005a05bbca086
root <zla236@sfu.ca>
2023-02-22 10:49:21 -0800

    resolve conflicts

--- include/efs/cli.h
+++ include/efs/cli.h
@@ -17,7 +17,7 @@ namespace Efs {
       std::string m_currentDir;
       std::string cd(std::string currentDir, std::string targetDir);
       void pwd(std::string currentDir, Database* database);
-      void ls(std::string currentDir);
+      void ls(std::string currentDir, Database* database);
       void cat(std::string currentDir, std::string filepath);
       void share(std::string currentDir, std::string filepath, std::string targetuser);
       void mkdir(std::string currentUser, std::string r_currentDir,

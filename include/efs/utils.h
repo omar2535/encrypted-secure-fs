04ffe452ca276deea94e9be923d4385fcf64fcc0
Omar <omar2535@alumni.ubc.ca>
2023-02-22 16:03:44 -0800

    Allow spaces in mkfile file input (#20)

--- include/efs/utils.h
+++ include/efs/utils.h
@@ -1,8 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 namespace Efs {
   class Utils {
-    std::string getFilenameFromFilepath(std::string filepath) {
-      std::string base_filename = path.substr(path.find_last_of("/\\") + 1)
-      return base_filename;
-    }
-  }
+    public:
+      // Gets filename from filepath
+      static std::string getFilenameFromFilepath(std::string filepath) {
+        std::string base_filename = filepath.substr(filepath.find_last_of("/\\") + 1);
+        return base_filename;
+      }
+
+      // joins a vector of string by the delimiter given
+      static std::string join(std::vector<std::string> const &strings, std::string delim) {
+        std::stringstream ss;
+        std::copy(strings.begin(), strings.end(), std::ostream_iterator<std::string>(ss, delim.c_str()));
+        return ss.str();
+      }
+  };
 }
